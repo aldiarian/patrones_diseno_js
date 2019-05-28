8c61024309caf5657fc9db4ce004d04bfbd7d58d
alberto diarian <alberto.diarian@unidadeditorial.es>
2019-05-28 15:09:15 +0200

    end patterns

--- patterns.js
+++ patterns.js
@@ -53,12 +53,12 @@ console.log ( `
 
         publicFunctions.suma = ( valor_a , valor_b ) => {
             let result = valor_a + valor_b;
-            console.log ( `el resultado de sumar ${valor_a} + ${valor_b} es: ${privateFunction(result)}` )
+            console.log ( `el resultado de sumar ${valor_a} + ${valor_b} es: ${_privateFunction(result)}` )
         };
 
         publicFunctions.resta = ( valor_a , valor_b ) => {
             let result = valor_a + valor_b;
-            console.log ( `el resultado de restar ${valor_a} - ${valor_b} es: ${privateFunction(result)}` )
+            console.log ( `el resultado de restar ${valor_a} - ${valor_b} es: ${_privateFunction(result)}` )
         }
 
         // devuelvo solamente los métodos públicos
@@ -68,8 +68,67 @@ console.log ( `
 
     MyModule.suma( 43,234)
     MyModule.resta( 343,34)
-    MyModule._privateFunction(34); // -->> ERROR no puede acceder al método privado, ya que no está dentro del return.
+    //MyModule._privateFunction(34);  -->> ERROR no puede acceder al método privado, ya que no está dentro del return.
 
     console.log(MyModule);
+
+})();
+
+
+(()=>{
+    console.log ( `
+    *************
+    Patrón Prototype
+    *************`
+)
+    // Patrón Prototype
+    // Este patrón de diseño tiene como finalidad crear nuevos objetos duplicándolos, clonando una instancia creada previamente, en resumen se podría decir que el uso de este patrón es lo más cercano a POO (Programación Orientada a Objetos), con los nuevas funciones de EmacScript 2016 esto puede armarce usando class, extends, etc.
+    
+    // Otra de las características es que todos los objetos en JavaScript cuentan con la propiedad proto, lo cual facilita crear nuevas funciones para clases ya existentes, prácticamente este patrón es la onda jejeje.
+    
+    // Para poder comprender mejor este patrón vamos a crear un ejemplo usando prototype y otro utilizando las nuevas funciones de emacScritpt.
+    
+    // Paso 1 : Creamos la clase Persona.
+
+    class Persona {
+        constructor( nombre, apellido ){
+            this.nombre = nombre,
+            this.apellido = apellido
+        }
+
+        getNombre(){
+            console.log( "Nombre: ",  this.nombre )
+        }
+        getApellido(){
+            console.log( "Apellido: ",  this.apellido )
+        }
+    }
+
+    let andres = new Persona( 'andres', 'sanchez');
+    andres.getNombre();
+    andres.getApellido();
+
+    console.log ( `
+    *************
+    class extends
+    *************`
+)
+
+    // Paso 2 : Extendiendo la clase Persona a una nueva podremos usar sus métodos y propiedades, además de incluir nuevas.
+
+    class Superlopez extends Persona {
+        constructor( nombre, apellido, superpoder ){ // tenemos que declarar las propiedades de Persona, y las nuevas que queramos tener
+            super( nombre, apellido ); // se llama al constructor de la clase Persona con sus propiedades ya declaradas
+            this.superpoder = superpoder;
+        }
+        getSuperlopez(){
+            this.getNombre()
+            this.getApellido();
+            console.log( 'tiene el superpoder :' , this.superpoder );
+        }
+    }
+
+    let heroe = new Superlopez ( 'Andrés', 'López', 'volar');
+    heroe.getSuperlopez();
 
 })();
